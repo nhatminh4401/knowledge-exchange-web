d7d976e3fddc2f4e7a8d6c9e42cdbcc9abe83278
nhatminh4401 <58182906+nhatminh4401@users.noreply.github.com>
2023-08-26 10:39:34 +0700

    feat: handle logic for questionDetail

--- src/components/Question/index.jsx
+++ src/components/Question/index.jsx
@@ -1,12 +1,12 @@
-import { Avatar, Badge, Card, Row } from 'antd';
-import './styles.css';
-import { LikeOutlined } from '@ant-design/icons';
-import moment from 'moment';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectToken } from '../../app/reducers/authReducer';
-import { reviewClient } from '../../api/client';
-import { useEffect, useState } from 'react';
+import { Avatar, Badge, Card, Row } from "antd";
+import "./styles.css";
+import { LikeOutlined } from "@ant-design/icons";
+import moment from "moment";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectToken } from "../../app/reducers/authReducer";
+import { reviewClient } from "../../api/client";
+import { useEffect, useState } from "react";
 
 const Question = ({
   title,
@@ -40,7 +40,7 @@ const Question = ({
           question_id: questionId,
           like: true,
           rating: 1,
-          report: '',
+          report: "",
         })
         .then((res) => {
           console.log(res.data);
@@ -56,27 +56,31 @@ const Question = ({
   }, []);
 
   return (
-    <Card className='question'>
-      <h3 className='question-title'>{title}</h3>
-      <div className='question-content'>{content}</div>
-      <ul className='tag-list'>
+    <Card className="question">
+      <Link to={`/question/${questionId}`} style={{ color: "black" }}>
+        <h3 className="question-title">{title}</h3>
+      </Link>
+      <div className="question-content">{content}</div>
+
+      <ul className="tag-list">
         {tags &&
           tags?.map((item, index) => {
             return (
-              <li key={index} className='tag-item'>
+              <li key={index} className="tag-item">
                 {item}
               </li>
             );
           })}
       </ul>
-      <Row justify='space-between' className='question-footer'>
-        <div className='question-author'>
+      <div className="question-category">{category_name}</div>
+      <Row justify="space-between" className="question-footer">
+        <div className="question-author">
           by
           {/* <div className="question-author__avatar"></div> */}
           <Link to={`/profile/${user_id}`}>
             <Avatar
-              icon=''
-              size={'30px'}
+              icon=""
+              size={"30px"}
               src={avatar}
               style={{ marginLeft: 5, marginRight: 5 }}
             >
@@ -84,24 +88,24 @@ const Question = ({
             </Avatar>
             {/* <div className="avatar">avatar</div> */}
           </Link>
-          <span className='question-author__name'>{username}</span>
-          <span className='question-author__time'>
+          <span className="question-author__name">{username}</span>
+          <span className="question-author__time">
             {moment(created_date).fromNow()}
           </span>
         </div>
-        <div className='question-like'>
+        <div className="question-like">
           <Badge
-            className='question-like__count'
+            className="question-like__count"
             count={likeCount > 0 ? Number(likeCount) : 0}
           />
-          <span className='question-like__icon'>
+          <span className="question-like__icon">
             <LikeOutlined
               onClick={() => {
                 if (!token) {
-                  alert('Unauthorized access to like, please login first!');
+                  alert("Unauthorized access to like, please login first!");
                 } else {
                   if (userLiked) {
-                    alert('You already liked this question!');
+                    alert("You already liked this question!");
                   } else {
                     handleLikeQuestion();
                   }
--- src/pages/QuestionDetail/index.jsx
+++ src/pages/QuestionDetail/index.jsx
@@ -1,67 +1,149 @@
-import { Button, Card, Col, Row } from 'antd';
-import Typography from 'antd/es/typography/Typography';
-import './styles.css';
-import Header from '../../components/Header';
-import Comment from '../../components/Comment';
+import { Button, Card, Col, Row } from "antd";
+import Typography from "antd/es/typography/Typography";
+import "./styles.css";
+import Header from "../../components/Header";
+import Comment from "../../components/Comment";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { QUESTION_API_URL } from "../../utils/constants";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setUser } from "../../app/reducers/authReducer";
+import { userClient } from "../../api/client";
+import moment from "moment";
+import { Link } from "react-router-dom";
+import { Avatar } from "antd";
 
 const QuestionDetail = () => {
+  const { id } = useParams();
+  const [questionData, setQuestionData] = useState({});
+  const dispatch = useDispatch();
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  };
+
+  useEffect(() => {
+    axios
+      .get(`${QUESTION_API_URL}/questions?id=${id}`, config)
+      .then((res) => {
+        console.log(res.data);
+        setQuestionData(res.data[0]);
+      })
+      .catch((err) => {
+        alert("Error: Question not exists", err);
+        setQuestionData([]);
+      });
+  }, [id]);
+
+  const token = localStorage.getItem(`token`);
+  useEffect(() => {
+    if (token) {
+      userClient.defaults.headers["Authorization"] = `Bearer ${token}`;
+      // reviewClient.defaults.headers["Authorization"] = `Bearer ${token}`;
+      userClient
+        .get("/user")
+        .then((res) => {
+          console.log(res.data);
+          dispatch(setUser(res.data));
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      // axios
+      //   .get(`${REVIEW_API_URL}/review-question?like`, config)
+      //   .then((res) => {
+      //     setReviewDataCount(res.data);
+      //   })
+      //   .catch((err) => {
+      //     console.error("Error fetching review data count: ", err);
+      //   });
+      // reviewClient
+      //   .get("/review")
+      //   .then((res) => {
+      //     console.log(res.data);
+      //     setReviewList([...res.data]);
+      //   })
+      //   .catch((err) => {
+      //     console.log(err);
+      //   });
+    } else {
+      delete userClient.defaults.headers["Authorization"];
+    }
+  }, [token, dispatch]);
+
   return (
     <>
       <Header />
-      <main className='wrapper'>
-        <div className='container'>
-          <Typography.Title level={1}>Question Title</Typography.Title>
-          <Typography.Title className='question-category' level={3}>
-            Category name
+      <main className="wrapper">
+        <div className="container">
+          <Typography.Title level={1}>{questionData?.title}</Typography.Title>
+          <Typography.Title className="question-category" level={3}>
+            {questionData?.category_name}
           </Typography.Title>
           <Row>
-            <div className='question-author'>
+            <div className="question-author">
               by
-              <div className='question-author__avatar'></div>
-              <span className='question-author__name'>Min</span>
-              <span className='question-author__time'>2 days ago</span>
+              <Link to={`/profile/${questionData?.user}`}>
+                <Avatar
+                  icon=""
+                  size={"30px"}
+                  src={questionData?.avatar}
+                  style={{ marginLeft: 5, marginRight: 5 }}
+                >
+                  {questionData?.username?.charAt(0).toUpperCase()}
+                </Avatar>
+                {/* <div className="avatar">avatar</div> */}
+              </Link>
+              <span className="question-author__name">
+                {questionData?.username}
+              </span>
+              <span className="question-author__time">
+                {moment(questionData?.created_date).fromNow()}
+              </span>
             </div>
           </Row>
           <Row>
-            <div className='question-content'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
-              inventore, quos sint quisquam autem veniam in dicta labore tempora
-              deserunt? Lorem ipsum dolor sit, amet consectetur adipisicing
-              elit. Iure ducimus cupiditate suscipit repellendus at assumenda
-              deserunt eligendi amet adipisci minima?
-            </div>
+            <div className="question-content">{questionData?.content}</div>
           </Row>
           <Row>
-            <ul className='tag-list'>
-              <li className='tag-item'>binh</li>
-              <li className='tag-item'>hieu</li>
-              <li className='tag-item'>cuong</li>
+            <ul className="tag-list">
+              {questionData?.tags &&
+                questionData?.tags?.map((item, index) => {
+                  return (
+                    <li key={index} className="tag-item">
+                      {item}
+                    </li>
+                  );
+                })}
             </ul>
           </Row>
-          <Row className='answer-section'>
+          <Row className="answer-section">
             <Typography.Title level={2}>2 answers</Typography.Title>
             <Col span={24}>
               <Card>
-                <div className='answer-author'>
-                  <div className='answer-author__avatar'></div>
-                  <span className='answer-author__name'>Min</span>
-                  <span className='answer-author__time'>25 Aug 2023</span>
+                <div className="answer-author">
+                  <div className="answer-author__avatar"></div>
+                  <span className="answer-author__name">Min</span>
+                  <span className="answer-author__time">25 Aug 2023</span>
                 </div>
-                <div className='answer-content'>
+                <div className="answer-content">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Odiol
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Sapiente, aut!
                 </div>
-                <ul className='comment-list'>
+                <ul className="comment-list">
                   <Comment />
                   <Comment />
                 </ul>
               </Card>
               <Col>
-                <form className='comment-form'>
-                  <div className='comment-form__title'>Comment</div>
-                  <textarea className='comment-form__textarea'></textarea>
-                  <Button className='comment-form__btn'>
+                <form className="comment-form">
+                  <div className="comment-form__title">Comment</div>
+                  <textarea className="comment-form__textarea"></textarea>
+                  <Button className="comment-form__btn">
                     Post your comment
                   </Button>
                 </form>
